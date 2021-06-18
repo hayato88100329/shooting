8390a63a9742df5986fdc07888d72042ecdf0ae1
遠藤　颯人 <66350205+hayato88100329@users.noreply.github.com>
2021-06-18 10:25:17 +0900

    弾が出る仕様追加

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -7,7 +7,16 @@ public static class Utils
 {
     // 移動可能な範囲
     public static Vector2 m_moveLimit = new Vector2(6.3f, 3.4f);
-
+    // 指定された角度（ 0 ～ 360 ）をベクトルに変換して返す
+    public static Vector3 GetDirection(float angle)
+    {
+        return new Vector3
+        (
+            Mathf.Cos(angle * Mathf.Deg2Rad),
+            Mathf.Sin(angle * Mathf.Deg2Rad),
+            0
+        );
+    }
     // 指定された位置を移動可能な範囲に収めた値を返す
     public static Vector3 ClampPosition(Vector3 position)
     {
@@ -27,4 +36,6 @@ public static class Utils
         var rad = Mathf.Atan2(dy, dx);
         return rad * Mathf.Rad2Deg;
     }
+
+    
 }

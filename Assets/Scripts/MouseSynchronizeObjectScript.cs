fd1a8603d3f8f95265800af42486bbd7983971f7
遠藤　颯人 <66350205+hayato88100329@users.noreply.github.com>
2021-07-16 16:08:44 +0900

    マウス操作2

--- Assets/Scripts/MouseSynchronizeObjectScript.cs
+++ Assets/Scripts/MouseSynchronizeObjectScript.cs
@@ -19,7 +19,7 @@ public class MouseSynchronizeObjectScript : MonoBehaviour
 		// Vector3でマウス位置座標を取得する
 		position = Input.mousePosition;
 		// Z軸修正
-		position.z = 5f;
+		position.z = 10f;
 		// マウス位置座標をスクリーン座標からワールド座標に変換する
 		screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
 		// ワールド座標に変換されたマウス座標を代入
